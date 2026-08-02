b27b13693a9a9a79ac57dfc3196f1c93e20de073
PlayfulAJ <jabaringure@gmail.com>
2026-08-02 21:21:43 +0300

    Implementing the JWT authentication and login

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, make_response
 from flask_migrate import Migrate # the migrate allows us to make changes to the database without losing data
-from flask_jwt_extended import JWTManager #JWT is a method of authentication that allows us to create secure tokens for users to acceess protected routes
+from flask_jwt_extended import JWTManager, create_access_token #JWT is a method of authentication that allows us to create secure tokens for users to acceess protected routes
 
 from models import db, bcrypt, User, Task
 
@@ -39,6 +39,39 @@ def signup():
         "username": user.username
     }, 201)
 
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    user = User.query.filter_by(
+        username=data["username"]
+    ).first()
+
+    if user is None:
+        return make_response(
+            {"error": "Invalid username or password"},
+            401
+        )
+
+    if not bcrypt.check_password_hash(
+        user.password_hash,
+        data["password"]
+    ):
+        return make_response(
+            {"error": "Invalid username or password"},
+            401
+        )
+
+    access_token = create_access_token(
+        identity=user.id
+    )
+
+    return make_response(
+        {
+            "access_token": access_token
+        },
+        200
+    )
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
